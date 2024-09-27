e2b55bcf13c570d52a9fcf32d0906cdac884b162
Rishav Singh <rishavrajcrj@gmail.com>
2024-09-27 13:27:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-// Copyright 2024 Nadim Kobeissi
-// Licensed under the WTFPL License
-
-// const fs = require(`fs`);
-// const path = require(`path`);
+const fs = require(`fs`);
+const path = require(`path`);
 
 async function main() {
+  console.log("Downloading...");
+
   const url =
     "https://storage.googleapis.com/panels-api/data/20240916/media-1a-i-p~s";
-
-  //   const delay = (ms) => {
-  //     return new Promise((resolve) => setTimeout(resolve, ms));
-  //   };
-
+  const delay = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -25,59 +22,43 @@ async function main() {
 
     console.log(data);
 
-    // const downloadDir = path.join(__dirname, 'downloads');
-    // if (!fs.existsSync(downloadDir)) {
-    // 	fs.mkdirSync(downloadDir);
-    // 	console.info(`📁 Created directory: ${downloadDir}`);
-    // }
-    // let fileIndex = 1;
-    // for (const key in data) {
-    // 	const subproperty = data[key];
-    // 	if (subproperty && subproperty.dhd) {
-    // 		const imageUrl = subproperty.dhd;
-    // 		console.info(`🔍 Found image URL!`);
-    // 		await delay(100);
-    // 		const ext = path.extname(new URL(imageUrl).pathname) || '.jpg';
-    // 		const filename = `${fileIndex}${ext}`;
-    // 		const filePath = path.join(downloadDir, filename);
-    // 		await downloadImage(imageUrl, filePath);
-    // 		console.info(`🖼️ Saved image to ${filePath}`);
-    // 		fileIndex++;
-    // 		await delay(250);
-    // 	}
-    // }
+    const downloadDir = path.join(__dirname, "downloads");
+    if (!fs.existsSync(downloadDir)) {
+      fs.mkdirSync(downloadDir);
+      console.info(`📁 Created directory: ${downloadDir}`);
+    }
+    let fileIndex = 1;
+    for (const key in data) {
+      const subproperty = data[key];
+      if (subproperty && subproperty.dhd) {
+        const imageUrl = subproperty.dhd;
+        console.info(`🔍 Found image URL!`);
+        await delay(100);
+        const ext = path.extname(new URL(imageUrl).pathname) || ".jpg";
+        const filename = `${fileIndex}${ext}`;
+        const filePath = path.join(downloadDir, filename);
+        await downloadImage(imageUrl, filePath);
+        console.info(`🖼️ Saved image to ${filePath}`);
+        fileIndex++;
+        await delay(250);
+      }
+    }
   } catch (error) {
     console.error(`Error: ${error.message}`);
   }
 }
 
-// async function downloadImage(url, filePath) {
-//   const response = await fetch(url);
-//   if (!response.ok) {
-//     throw new Error(`Failed to download image: ${response.statusText}`);
-//   }
-//   const arrayBuffer = await response.arrayBuffer();
-//   const buffer = Buffer.from(arrayBuffer);
-//   await fs.promises.writeFile(filePath, buffer);
-// }
-
-// function asciiArt() {
-//   console.info(`
-//  /$$      /$$ /$$   /$$ /$$$$$$$   /$$$$$$  /$$$$$$$
-// | $$$    /$$$| $$  /$$/| $$__  $$ /$$__  $$| $$__  $$
-// | $$$$  /$$$$| $$ /$$/ | $$  \\ $$| $$  \\__/| $$  \\ $$
-// | $$ $$/$$ $$| $$$$$/  | $$$$$$$ |  $$$$$$ | $$  | $$
-// | $$  $$$| $$| $$  $$  | $$__  $$ \\____  $$| $$  | $$
-// | $$\\  $ | $$| $$\\  $$ | $$  \\ $$ /$$  \\ $$| $$  | $$
-// | $$ \\/  | $$| $$ \\  $$| $$$$$$$/|  $$$$$$/| $$$$$$$/
-// |__/     |__/|__/  \\__/|_______/  \\______/ |_______/`);
-//   console.info(``);
-//   console.info(
-//     `🤑 Starting downloads from your favorite sellout grifter's wallpaper app...`
-//   );
-// }
+async function downloadImage(url, filePath) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to download image: ${response.statusText}`);
+  }
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
+  await fs.promises.writeFile(filePath, buffer);
+}
 
 (() => {
   //   asciiArt();
-  main();
+  setTimeout(main, 5000);
 })();
